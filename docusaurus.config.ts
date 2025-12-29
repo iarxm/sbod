265b278e2453afcd2aab80b304fc112d1eca6e00
Iarom Madden <mail@iarom.org>
2025-12-29 14:50:25 +0000

    m

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,18 +10,11 @@ const config: Config = {
     v4: true,
   },
   url: 'https://iarxm.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/sbod/',
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'iarxm', // Usually GitHub org/user name.
-  projectName: 'sbod', // Usually repo name.
+  organizationName: 'iarxm',
+  projectName: 'sbod',
   trailingSlash: false,
   onBrokenLinks: 'throw',
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
