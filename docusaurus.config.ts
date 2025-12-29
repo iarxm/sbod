9f4148888083cb7fa8ab99ec5c7fd3a0b6d8b8c4
Iarom Madden <mail@iarom.org>
2025-12-29 12:25:27 +0000

    fix base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   future: {
     v4: true,
   },
-  url: 'https://sbod.github.io',
+  url: 'https://iarxm.github.io/sbod/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
