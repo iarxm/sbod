47ae61e3e20122562ed5a2c87f8915356d918afa
Iarom Madden <mail@iarom.org>
2025-12-29 12:52:13 +0000

    m

---
+++ docs/Guide/guide.md
@@ -0,0 +1,7 @@
+---
+id: guide
+aliases: []
+tags: []
+---
+
+#
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'iarxm', // Usually GitHub org/user name.
   projectName: 'sbod', // Usually repo name.
-  trailingSlash: false,
+  //trailingSlash: false,
   onBrokenLinks: 'throw',
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
